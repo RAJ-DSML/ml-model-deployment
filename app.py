b1b8fda6e91f2708034804256f8ff346d527e255
RAJ-DSML <raj80133@gmail.com>
2022-09-18 17:41:07 +0530

    add submit function

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/sub", methods=["POST"])
+def submit():
+    # HTML form data
+    if request.method == "POST":
+        name = request.form["USERNAME"]
+    
+    # .py file data from HTML form
+    return render_template("sub.html", name=name)
+
 if __name__ == "__main__":
     app.run()
