b0bd660e0798330326d51cbb5b069fc68703d1c6
RAJ-DSML <raj80133@gmail.com>
2022-09-18 21:24:33 +0530

    app.py modified

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
+from flask  import Flask, request, render_template
+import pickle
 
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return render_template("index.html")
 
-@app.route("/sub", methods=["POST"])
-def submit():
-    # HTML form data
-    if request.method == "POST":
-        name = request.form["USERNAME"]
-    
-    # .py file data from HTML form
-    return render_template("sub.html", name=name)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Heart Disease Percentage is {}'.format(output))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
